2071974dae23b9c8b567d22d7d4081da179f2701
Auxenlinours <a.desc.pro@gmail.com>
2024-01-17 14:08:00 +0100

    adresse

--- Application/Odoo.py
+++ Application/Odoo.py
@@ -2,7 +2,7 @@ import xmlrpc.client
 from tkinter import Tk
 class ErpOdoo:
     def __init__(self):
-        self.url = 'http://192.168.56.101:8069'
+        self.url = 'http://172.31.11.122:8069'
         self.db = 'MyFactory'
         self.models = None  # Initialisation à None, sera configuré lors de la connexion
         self.uid = None  # Initialisation à None, sera configuré lors de la connexion
